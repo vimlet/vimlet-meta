17ab80055fa9d1764c7de4d369042fc0e9c07088
= <=>
2017-10-01 15:42:04 +0200

    Testing format on pre-commit

--- src/node/release/index.js
+++ src/node/release/index.js
@@ -3,7 +3,7 @@
 "use strict";
 
 // Require
-var generator = require("./lib/base");
+var generator = require('./lib/base');
 var generatorInstance = generator.instance();
 
 // Generator node config
