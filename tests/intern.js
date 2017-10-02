2872fda708cd802ad5f4cc9f8550731e7a96538e
lethe <lethe@mail.com>
2017-10-02 13:59:58 +0200

    Added testing example

---
+++ tests/intern.js
@@ -0,0 +1,30 @@
+define({
+
+	loaderOptions: {
+		// Packages that should be registered with the loader in each testing environment
+		packages: [
+			{ name: "tests", location: "./tests" }
+		]
+	},
+
+	// Unit test suite(s) to run in node
+	suites: [
+		"dojo/has!host-node?tests/unit/test1.js"
+	],
+
+	// Functional test suite(s) to execute against each browser once unit tests are completed
+	functionalSuites: [
+
+	],
+
+	// https://theintern.io/intern/#reporter-results
+	// https://theintern.io/intern/#custom-reporters
+	reporters: [
+		// { id: "console" },
+		{ id: "Pretty" },
+		{ id: 'JUnit', filename: 'tests/report.xml' }
+	],
+
+	excludeInstrumentation: /^(?:tests|node_modules)\//
+
+});
